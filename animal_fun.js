bec519871428a343b5bd3d66ec93e34c3b3c705d
Mark Azuolas <markazuolas@gmail.com>
2017-09-22 16:39:08 -0700

    comment out code

--- animal_fun.js
+++ animal_fun.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
-fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-  if (err){
-    console.log(err);
-    return
-  }
-  console.log(data);
-
-})
-
-fs.writeFile('./example.txt', 'Hello World', (err) => {
-  console.log(err);
-});
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err){
+//     console.log(err);
+//     return
+//   }
+//   console.log(data);
+//
+// })
+//
+// fs.writeFile('./example.txt', 'Hello World', (err) => {
+//   console.log(err);
+// });
