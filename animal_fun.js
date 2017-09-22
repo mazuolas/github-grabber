7719a1ee752373819d286478a5eacfabb1a4c5da
Mark Azuolas <markazuolas@gmail.com>
2017-09-22 16:35:28 -0700

    convert input to text

--- animal_fun.js
+++ animal_fun.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-fs.readFile('./animals.txt', (err, data) => {
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
   if (err){
     console.log(err);
     return
   }
-  console.log(data);  
+  console.log(data);
 
 })
