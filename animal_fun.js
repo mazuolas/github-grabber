b260517084614907bc816acb8b4e74907ae38129
Mark Azuolas <markazuolas@gmail.com>
2017-09-22 17:00:49 -0700

    comment out example

--- animal_fun.js
+++ animal_fun.js
@@ -14,20 +14,20 @@ const fs = require('fs');
 // });
  // console.log(process.argv);
 
-fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-  if (err){
-    console.log(err);
-    return;
-  }
-  const searchLetter = process.argv[2];
-  const animals = data.split('\n');
-  let returnAnimals = [];
-  animals.forEach((animal) =>{
-    if (animal[0].toUpperCase() === searchLetter.toUpperCase()){
-      returnAnimals.push(animal);
-    }
-  });
-  fs.writeFile(`${searchLetter}_animals.txt`, returnAnimals.join('\n'), (err) =>{
-    console.log(err);
-  });
-});
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err){
+//     console.log(err);
+//     return;
+//   }
+//   const searchLetter = process.argv[2];
+//   const animals = data.split('\n');
+//   let returnAnimals = [];
+//   animals.forEach((animal) =>{
+//     if (animal[0].toUpperCase() === searchLetter.toUpperCase()){
+//       returnAnimals.push(animal);
+//     }
+//   });
+//   fs.writeFile(`${searchLetter}_animals.txt`, returnAnimals.join('\n'), (err) =>{
+//     console.log(err);
+//   });
+// });
